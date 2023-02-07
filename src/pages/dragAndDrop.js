60828147bcfb714ac3e9afcb2ad7afb56bc9693b
Killian JF <killian.jaffrelot@mmibordeaux.com>
2023-02-07 16:28:49 +0100

    dnd V3

--- src/pages/dragAndDrop.js
+++ src/pages/dragAndDrop.js
@@ -10,11 +10,11 @@ const ItemList = [
     },
     {
         id: 2,
-        url: "./logo192.png"
+        url: "https://storage.googleapis.com/support-kms-prod/ZAl1gIwyUsvfwxoW9ns47iJFioHXODBbIkrK"
     },
     {
         id: 3,
-        url: "./logo192.png"
+        url: "https://www.facebook.com/images/fb_icon_325x325.png"
     }
 ]
 
@@ -32,24 +32,42 @@ function DragAndDrop() {
 
     const [{ isOver }, drop] = useDrop(() => ({
         accept: "image",
-        drop1: (item) => AddImageToBoard(item.id),
+        drop: (item) => AddImageToBoard(item.id,1),
         collect: (monitor) => ({
             isOver: !!monitor.isOver(),
         })
     }))
+    const [{ isOver2 }, drop2] = useDrop(() => ({
+        accept: "image",
+        drop: (item) => AddImageToBoard(item.id,2),
+        collect: (monitor) => ({
+            isOver2: !!monitor.isOver(),
+        })
+    }))
+    const [{ isOver3 }, drop3] = useDrop(() => ({
+        accept: "image",
+        drop: (item) => AddImageToBoard(item.id,3),
+        collect: (monitor) => ({
+            isOver3: !!monitor.isOver(),
+        })
+    }))
+    const AddImageToBoard = (itemId,boardId) => {
+        const pictureList = ItemList.filter((item) => itemId === item.id); // récupère l'objet de l'ID = itemId
+        console.log("itemId : ", itemId, " boardId : ",boardId);
+        if (itemId === 1 & boardId === 1) {
+            setBoard((board) => [...board, pictureList[0]]);
+            ItemList.splice(itemId-1,1,"")
+        }
+        else if (itemId === 2 & boardId === 2) {
+            setBoard2((board2) => [...board2, pictureList[0]]);
+            ItemList.splice(itemId-1,1,"")
+        }
+        else if (itemId === 3 & boardId === 3) {
+            setBoard3((board3) => [...board3, pictureList[0]]);
+            ItemList.splice(itemId-1,1,"")
+        }
 
-    const AddImageToBoard = (id) => {
-        const pictureList = ItemList.filter((item) => id === item.id);
-
-        // if (id === 1) {
-             setBoard((board) => [...board, pictureList[0]]);
-        // }
-        // else if (id === 2) {
-        //     setBoard2((board2) => [...board2, pictureList[0]]);
-        // }
-        // else if (id === 3) {
-        //     setBoard3((board3) => [...board3, pictureList[0]]);
-        // }
+        console.log("ItemList = ", ItemList)
 
 
     }
@@ -67,15 +85,15 @@ function DragAndDrop() {
                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
                     })}
                 </div>
-                <div className={styles.board2} ref={drop}>
+                <div className={styles.board2} ref={drop2}>
                 <h3>Board 2</h3>
-                    {board.map((item) => {
+                    {board2.map((item) => {
                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
                     })}
                 </div>
-                <div className={styles.board3} ref={drop}>
+                <div className={styles.board3} ref={drop3}>
                 <h3>Board 3</h3>
-                    {board.map((item) => {
+                    {board3.map((item) => {
                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
                     })}
                 </div>
