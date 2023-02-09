b20c2fccf6c5a0a6c1042ae0540fceceb35fedfa
Killian JF <killian.jaffrelot@mmibordeaux.com>
2023-02-09 11:55:03 +0100

    drawV2

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -4,41 +4,38 @@ function sketch(p) {
     var canvasDiv = document.getElementById('test');
     var width = canvasDiv.offsetWidth;
     var height = canvasDiv.offsetHeight;
-    console.log(width,height);
+    //console.log(width,height);
 
 
 
     p.setup = function () {
         p.createCanvas(width,height );
-        p.background(0);
-        p.button = p.createButton("gomme");
-        p.button.size(50,30);
-        p.button.position(200,10);
-        p.button.mouseClicked(p.gomme);
+        p.background('white');
+        p.img = p.loadImage('./gomme.png');
       
         p.button1 = p.createButton("save");
-        p.button1.size(50,30);
-        p.button1.position(260,10);
+        p.button1.size(width/3,width/9);
+        p.button1.position(width*0.75,height*0.1);
         p.button1.mouseClicked(p.exporter);
-      
-      
+        p.c = "#DC7753";
+        p.strokeWeight(10)
       
     
     };
+    // p.windowResized = function(){
+    //   p.resizeCanvas(width, height,[p.noredraw]);
+    // }
 
 
     p.exporter = function () {
         p.save("myImage.jpg")
     }
     
-    p.gomme = function () {
-        p.c = "white";
-    }
-
     p.draw = function() {
+      
         //console.log(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY)
-    
-    
+      
+        p.image(p.img,width*0.1-20, height-height*0.05-20, 40, 40);
         if (p.mouseIsPressed) {
             p.stroke(p.c);
               p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
@@ -46,39 +43,51 @@ function sketch(p) {
       
         p.noStroke();
         //draw the first button
-        p.fill("red");
-        p.ellipse(20, 20, 40, 40);
+        p.fill("#DC7753");
+        p.ellipse(width*0.3, height-height*0.05, 40, 40);
       
         //draw the second button
-        p.fill("blue");
-        p.ellipse(60, 20, 40, 40);
-    
-    //draw the  second button
-    p.fill("yellow");
-    p.ellipse(100, 20, 40, 40);
+        p.fill("#71A9BB");
+        p.ellipse(width*0.5, height-height*0.05, 40, 40);
     
         //draw the  second button
-        p.fill("green");
-        p.ellipse(140, 20, 40, 40);
+        p.fill("#D9C5AD");
+        p.ellipse(width*0.7, height-height*0.05, 40, 40);
+    
+        //draw the  second button
+        p.fill("#AD9669");
+        p.ellipse(width*0.9, height-height*0.05, 40, 40);
       }
+     
+      
       
       //this will run whenever the mouse is pressed
       p.mousePressed = function() {
-        if (p.mouseX > 0 && p.mouseX < 40 && p.mouseY > 0 && p.mouseY < 40) {
+        if (p.mouseX > width*0.3-20 && p.mouseX < width*0.3+20 && p.mouseY > height-height*0.05 - 20 && p.mouseY < height-height*0.05 + 20) {
           //set the variables to random values
-          p.c = "red";
+          p.c = "#DC7753";
+          p.strokeWeight(10)
         }
-        if (p.mouseX > 40 && p.mouseX < 80 && p.mouseY > 0 && p.mouseY < 40) {
+        if (p.mouseX > width*0.5-20 && p.mouseX < width*0.5+20 && p.mouseY > height-height*0.05 - 20 && p.mouseY < height-height*0.05 + 20) {
           //set the variables to random values
-          p.c = "blue";
+          p.c = "#71A9BB";
+          p.strokeWeight(10)
         }
-        if (p.mouseX > 80 && p.mouseX < 120 && p.mouseY > 0 && p.mouseY < 40) {
+        if (p.mouseX > width*0.7-20 && p.mouseX < width*0.7+20 && p.mouseY > height-height*0.05 - 20 && p.mouseY < height-height*0.05 + 20) {
             //set the variables to random values
-            p.c = "yellow";
+            p.c = "#D9C5AD";
+            p.strokeWeight(10)
           }
-          if (p.mouseX > 120 && p.mouseX < 160 && p.mouseY > 0 && p.mouseY < 40) {
+          if (p.mouseX > width*0.9-20 && p.mouseX < width*0.9+20 && p.mouseY > height-height*0.05 - 20 && p.mouseY < height-height*0.05 + 20) {
             //set the variables to random values
-            p.c = "green";
+            p.c = "#AD9669";
+            p.strokeWeight(10)
+          }
+
+          if (p.mouseX > width*0.1-20 && p.mouseX < width*0.1+20 && p.mouseY > height-height*0.05 - 20 && p.mouseY < height-height*0.05 + 20) {
+            //set the variables to random values
+            p.c = "white";
+            p.strokeWeight(40)
           }
 
 
